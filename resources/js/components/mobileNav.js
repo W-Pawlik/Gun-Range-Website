56ae1002178c9c7e6312d41902d10e004ee0d62c
Wojtek Pawlik <wojtek.pawlik17@gmail.com>
2024-07-03 23:38:13 +0200

    Implemented moblie navigation
    Created moblie nav that is implemented in moblieNav  module.

---
+++ resources/js/components/mobileNav.js
@@ -0,0 +1,22 @@
+`use strict`;
+
+export function mobileNav() {
+  const openNavBtnEl = document.querySelector('.btn-mobile-nav');
+  const headerEl = document.querySelector('.header');
+  const navLinksEl = document.querySelectorAll('.nav-link');
+
+  openNavBtnEl.addEventListener('click', e => {
+    if (headerEl.classList.contains('nav-open')) {
+      headerEl.classList.remove('nav-open');
+    } else {
+      headerEl.classList.add('nav-open');
+    }
+  });
+
+  navLinksEl.forEach(link => {
+    console.log(link);
+    link.addEventListener('click', e => {
+      headerEl.classList.remove('nav-open');
+    });
+  });
+}
--- resources/js/index.js
+++ resources/js/index.js
@@ -2,9 +2,12 @@
 import { slider } from './components/slider.js';
 import { map } from './components/map.js';
 import { hideAnimations } from './components/hideAnimations.js';
+import { mobileNav } from './components/mobileNav.js';
 
 slider();
 
 map();
 
 hideAnimations();
+
+mobileNav();
